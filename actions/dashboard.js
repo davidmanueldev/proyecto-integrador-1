d93cc5e7c2088c79a0bd53663060ba8c1fde0432
David Manuel <davidmanuel.dev@gmail.com>
2025-04-18 22:57:15 -0400

    Create Account Backend Logic

---
+++ actions/dashboard.js
@@ -0,0 +1,79 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+};
+
+export async function createAccount(data) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      throw new Error("Usuario no autorizado");
+    }
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    // Convertir el balance a número
+
+    const balanceFloat = parseFloat(data.balance);
+
+    if (isNaN(balanceFloat)) {
+      throw new Error("El balance debe ser un número");
+    }
+
+    // Verificar si el usuario ya tiene una cuenta
+
+    const existingAccounts = await db.account.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const shouldBeDefault =
+      existingAccounts.length === 0 ? true : data.isDefault;
+
+    // Si esta cuenta es la primera, marcarla como predeterminada y desactivar las demás
+    if (shouldBeDefault) {
+      await db.account.updateMany({
+        where: {
+          userId: user.id,
+          isDefault: true,
+        },
+        data: {
+          isDefault: false,
+        },
+      });
+    }
+
+    const account = await db.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldBeDefault,
+      },
+    });
+
+    const serializedAccount = serializeTransaction(account);
+
+    revalidatePath("/dashboard");
+
+    return { success: true, data: serializedAccount };
+  } catch (error) {
+    throw new Error("Error al crear la cuenta: " + error.message);
+  }
+}
