0b11a95248390be4b2706698f8f0d5e98bc45f8a
David Manuel <davidmanuel.dev@gmail.com>
2025-04-19 15:50:54 -0400

    Account Page

--- actions/accounts.js
+++ actions/accounts.js
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { date } from "zod";
 
 const serializeTransaction = (obj) => {
   const serialized = { ...obj };
@@ -49,4 +50,36 @@ export async function updateDefaultAccount(accountId) {
   } catch (error) {
     return { success: false, error: error.message };
   }
+}
+
+export async function getAccountWithTransactions(accountId) {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Usuario no autorizado");
+  }
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+  if (!user) {
+    throw new Error("Usuario no encontrado");
+  }
+
+  const account = await db.account.findUnique({
+    where: { id: accountId, userId: user.id },
+    include: {
+      transactions: {
+        orderBy: { date: "desc" },
+      },
+      _count: {
+        select: { transactions: true },
+      },
+    },
+  });
+
+  if (!account) return null;
+
+  return {
+    ...serializeTransaction(account),
+    transactions: account.transactions.map(serializeTransaction),
+  };
 }
